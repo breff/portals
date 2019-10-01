f9b99ec616b11607820ba08a1e596dbd2f72a111
Bref Murdoch <breffni.murdoch@viasat.com>
2019-10-01 14:29:32 +0100

    init

---
+++ packages/ifc-components/index.js
@@ -0,0 +1,4 @@
+var getVersion = function() {
+  return "v1.0.0 :) of IFC";
+};
+module.exports = getVersion;
---
+++ packages/ife-components/index.js
@@ -0,0 +1,4 @@
+var getVersion = function() {
+  return "v1.0.0 :) of IFE";
+};
+module.exports = getVersion;
